4abb51cc04e4301fb34d73a8e6c4fbb54e75949b
Sára Hölclová <sara.holclova@gmail.com>
2024-05-02 14:08:09 +0200

    Komponenta a fce map pro zobrazení seznamu

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,31 +1,43 @@
-import { render } from "@czechitas/render";
-import "../global.css";
-import "./index.css";
+import { render } from '@czechitas/render';
+import '../global.css';
+import './index.css';
+import { Episode } from '../components/Episode';
 
 const podcast1 = {
-  num: "126",
-  title: "Robot, který snědl koblihu",
-  guest: "Radovan Siwek",
+  num: '126',
+  title: 'Robot, který snědl koblihu',
+  guest: 'Radovan Siwek',
 };
 
-document.querySelector("#root").innerHTML = render(
-  <>
-    <div className="episodes-list">
-      <div className="episode">
-        <div className="episode__num">127</div>
-        <div className="episode__body">
-          <div className="episode__title">Hledání plyšového Yettiho</div>
-          <div className="episode__guest">host: Vojtěch Ryba</div>
-        </div>
-      </div>
+const episodes = [
+  { num: 126, title: 'Robot, který snědl koblihu', guest: 'Radovan Siwek' },
+  { num: 127, title: 'Hledání plyšového Yettiho', guest: 'Vojtěch Ryba' },
+  { num: 128, title: 'Moderní hrachová polévka', guest: 'Kamila Štancová' },
+  { num: 129, title: 'Poloautomatické zrcadlo', guest: 'Janka Janovská' },
+  { num: 130, title: 'Máčené hlavy parlamentu', guest: 'Jonáš Daněk' },
+  { num: 131, title: 'Služby na hraně přívěsu', guest: 'Tereza Křivánková' },
+  { num: 132, title: 'Klasifikace sněžných klokanů', guest: 'Josef Stix' },
+  { num: 133, title: 'Rybolov v Oceánu bouří', guest: 'Ludmila Gajová' },
+];
 
-      <div className="episode">
-        <div className="episode__num">128</div>
-        <div className="episode__body">
-          <div className="episode__title">Moderní hrachová kaše</div>
-          <div className="episode__guest">host: Kamila Štancová</div>
-        </div>
-      </div>
-    </div>
-  </>
+// const kazdaEpizoda = episodes.map((jednaEpizoda) => {
+//   return (
+//     <Episode
+//       num={jednaEpizoda.num}
+//       title={jednaEpizoda.title}
+//       guest={jednaEpizoda.guest}
+//     />
+//   );
+// });
+
+document.querySelector('#root').innerHTML = render(
+  episodes.map((jednaEpizoda) => {
+    return (
+      <Episode
+        num={jednaEpizoda.num}
+        title={jednaEpizoda.title}
+        guest={jednaEpizoda.guest}
+      />
+    );
+  }),
 );
